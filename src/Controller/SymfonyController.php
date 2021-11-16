a97d5419d467b205b71f1f33f1854d934aa02b73
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2021-11-16 15:49:39 +0100

    Ajout des twigs et de AbstractController

--- src/Controller/SymfonyController.php
+++ src/Controller/SymfonyController.php
@@ -2,12 +2,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
 
-class SymfonyController
+class SymfonyController extends AbstractController
 {
-    public function Main()
+    public function Main(Request $request)
     {
-        return new Response("Bonjour");
+        // Declaration et initialisation de $titre
+        $titre = "Liste des utilisateurs";
+        // Déclaration et initialisation du tableau stockant une liste d'utilisateur
+        $listeUtilisateur = ["Jean", "Pierre", "Louis", "Olivier"];
+        // Appel de la méthode render avec deux parametre le titre et le array
+        return $this->render("bonjour.html.twig", ["titre" => $titre, "array" => $listeUtilisateur]);
     }
 }
 
